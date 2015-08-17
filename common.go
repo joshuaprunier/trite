ee15c05a5459e7034333d23d900e7bfe455e214b
Joshua Prunier <jprunier@netprospex.com>
2015-08-17 15:45:06 -0400

    custom readPassword() replaced by sub repo ssh/terminal

--- common.go
+++ common.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"os"
 	"strings"
-	"syscall"
-	"unsafe"
+
+	"golang.org/x/crypto/ssh/terminal"
 
 	_ "github.com/go-sql-driver/mysql" // Go MySQL driver
 )
@@ -67,8 +68,13 @@ func (dbi *mysqlCredentials) connect() (*sql.DB, error) {
 	// If password is blank prompt user
 	if dbi.pass == "" {
 		fmt.Println("Enter password: ")
-		pwd, err := readPassword(0)
-		checkErr(err)
+		pwd, err := terminal.ReadPassword(int(os.Stdin.Fd()))
+		if err != nil {
+			if err != io.EOF {
+				checkErr(err)
+			}
+		}
+
 		dbi.pass = string(pwd)
 	}
 
@@ -87,52 +93,4 @@ func (dbi *mysqlCredentials) connect() (*sql.DB, error) {
 	err = db.Ping()
 
 	return db, err
-}
-
-// ReadPassword is borrowed from the crypto/ssh/terminal sub repo to accept a password from stdin without local echo.
-// http://godoc.org/code.google.com/p/go.crypto/ssh/terminal#Terminal.ReadPassword
-func readPassword(fd int) ([]byte, error) {
-	var oldState syscall.Termios
-	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), syscall.TCGETS, uintptr(unsafe.Pointer(&oldState)), 0, 0, 0); err != 0 {
-		return nil, err
-	}
-
-	newState := oldState
-	newState.Lflag &^= syscall.ECHO
-	newState.Lflag |= syscall.ICANON | syscall.ISIG
-	newState.Iflag |= syscall.ICRNL
-	if _, _, err := syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), syscall.TCSETS, uintptr(unsafe.Pointer(&newState)), 0, 0, 0); err != 0 {
-		return nil, err
-	}
-
-	defer func() {
-		syscall.Syscall6(syscall.SYS_IOCTL, uintptr(fd), syscall.TCSETS, uintptr(unsafe.Pointer(&oldState)), 0, 0, 0)
-	}()
-
-	var buf [16]byte
-	var ret []byte
-	for {
-		n, err := syscall.Read(fd, buf[:])
-		if err != nil {
-			return nil, err
-		}
-
-		if n == 0 {
-			if len(ret) == 0 {
-				return nil, io.EOF
-			}
-			break
-		}
-
-		if buf[n-1] == '\n' {
-			n--
-		}
-
-		ret = append(ret, buf[:n]...)
-		if n < len(buf) {
-			break
-		}
-	}
-
-	return ret, nil
 }
